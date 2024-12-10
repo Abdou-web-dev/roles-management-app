fc56d41f7be26a203ae1aa73019ea1e2d6cc13e6
Sifelhak Abdelmounim <sifelhak.abdelmounim@gmail.com>
2024-12-10 11:32:17 +0100

    fix: add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 
 export default defineConfig({
+  base: "/roles-management-app/", // Set the base path to the repository name
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     // Using '/api' as the prefix to match your request
-  //     "/api": {
-  //       target: "https://googlereviewsolicitor.azurewebsites.net",
-  //       changeOrigin: true,
-  //       secure: true, // Enables HTTPS, which might be necessary
-  //       rewrite: (path) => path.replace(/^\/api/, ""), // Removes `/api` prefix
-  //     },
-  //   },
-  // },
 });
